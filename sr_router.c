285af493dae65db9ab85423d49a44b9c53d24e9d
Jiamin <cjmyezi@gmail.com>
2013-05-22 22:05:18 -0700

    bf

--- sr_router.c
+++ sr_router.c
@@ -190,7 +190,7 @@ void handle_ip(struct sr_instance *sr, uint8_t * pckt, unsigned int len, char* i
   sr_ip_hdr_t *ip_hdr;
 
   ip_hdr = (sr_ip_hdr_t *)(pckt+sizeof(sr_ethernet_hdr_t));
-  uint16_t e_cksm = cksum(ip_hdr, ip_hdr->ip_len);
+  uint16_t e_cksm = cksum(ip_hdr, ip_hdr->ip_hl*4);
   uint16_t r_cksm = ip_hdr->ip_sum;
 
   fprintf(stderr, "%u %u\n", e_cksm,r_cksm);
